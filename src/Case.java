080109ad1c6bba3a52988db5e0e8f1a0cd1b8517
killian.carrat@etu.univ-orleans.fr <killian.carrat@etu.univ-orleans.fr>
2024-03-15 15:08:36 +0100

    classe Plateau/Case/Demineur/CaseIntelligente passe la compilation

---
+++ src/Case.java
@@ -0,0 +1,37 @@
+public class Case {
+    protected boolean contientUneBombe;
+    protected boolean estDecouverte;
+    protected boolean estMarquee;
+
+    public Case(){
+
+    }
+
+    public void reset(){
+
+    }
+
+    public void poseBombe(){
+
+    }
+
+    public boolean getContientUneBombe(){
+        return true;
+    }
+
+    public boolean getEstDecouverte(){
+        return true;
+    }
+
+    public boolean getEstMarquee(){
+        return true;
+    }
+
+    public boolean reveler(){
+        return true;
+    }
+
+    public void marquer(){
+
+    }
+}
